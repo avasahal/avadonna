29b8811d6190c0c9ad6f12b25dd1cd07bddaaed2
Ava Sahal <avasahal@Avas-MacBook-Air.local>
2018-08-08 17:47:25 +0300

    first commit

---
+++ AvaResume/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Inherits="AvaResume.Global" %>
---
+++ AvaResume/Global.asax.cs
@@ -0,0 +1,11 @@
+﻿using System.Web;
+
+namespace AvaResume
+{
+    public class Global : HttpApplication
+    {
+        protected void Application_Start()
+        {
+        }
+    }
+}
